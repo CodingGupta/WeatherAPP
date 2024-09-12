474a4d7a122a7a0ba38ab505daa47a9e8755b6d4
Raju Gupta <117964173+CodingGupta@users.noreply.github.com>
2024-09-12 16:51:08 +0530

    Update index.js

--- WeatherWebAPP/index.js
+++ WeatherWebAPP/index.js
@@ -9,7 +9,7 @@ const userInfoContainer = document.querySelector(".user-info-container");
 const errorContainer = document.querySelector(".error-container");
 
 let oldTab = userTab;
-const API_KEY = "d1845658f92b31c64bd94f06f7188c9c";
+
 oldTab.classList.add("current-tab");
 getFromSessionStorage();
 
